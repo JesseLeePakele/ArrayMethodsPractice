77570a827fcbc706bddaae3bd80932762734e078
Jesse-Lee Pakele <jesse-leepakele@Jesse-Lees-Air.hawaii.rr.com>
2019-04-24 11:13:53 -1000

    ArrayMethodsPractice

---
+++ app.js
@@ -0,0 +1,14 @@
+var myGrateful = ["Family","Food"];
+myGrateful.pop();
+myGrateful.push("Friends");
+myGrateful.shift("Food");
+myGrateful.unshift("Food");
+console.log(myGrateful);
+
+
+var proudSchool = ["Coding","Learning"];
+proudSchool.pop();
+proudSchool.push("Finishing Whack-o-Mole");
+proudSchool.shift("Learning");
+proudSchool.unshift("Learning");
+console.log(proudSchool);
